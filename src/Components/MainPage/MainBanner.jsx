b7fb98c24acb51d42f2b34c530ffd56abeea6208
codakawa <tynchtyk12264@gmail.com>
2023-12-27 16:23:01 +0600

    change logo

--- src/Components/MainPage/MainBanner.jsx
+++ src/Components/MainPage/MainBanner.jsx
@@ -1,5 +1,4 @@
 import React, { useContext, useEffect, useRef, useState } from 'react'
-import img from "../../assets/denjiBG.jpg"
 import { Link } from 'react-router-dom'
 import TrailerContext from '../../Context/TrailerContext'
 

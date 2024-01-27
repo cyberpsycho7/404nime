d13deed0d8b62df743073422af052ed4f27fb9b7
codakawa <tynchtyk12264@gmail.com>
2024-01-27 16:37:50 +0600

    profile link

--- src/Pages/MainPage.jsx
+++ src/Pages/MainPage.jsx
@@ -11,6 +11,7 @@ import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 import 'swiper/css/effect-fade';
 import ErrorPage from './ErrorPage'
+import { Link } from 'react-router-dom'
 
 
 const MainPage = ({currentWidth}) => {
@@ -267,7 +268,9 @@ const MainPage = ({currentWidth}) => {
           ) : null}
 
           <h3 className="text-2xl 1480res:px-[28px] px-2 font-medium 500res:mb-3 500res:mt-16 mb-5 mt-20">
-            Most Popular
+            <Link to={"/profile/touchme"}>
+              Most Popular
+            </Link>
           </h3>
           {popular ? (
             <SwiperComponent
